467b0769a7c3d00be6ef4431269c9b2c995d7900
bkunzh <dreamkunk@foxmail.com>
2020-10-09 12:20:44 +0800

    actuator

--- spring-cloud-samples/cloud-config-client/src/main/java/com/bkunzh/springcloud/configdemo/service/GitController.java
+++ spring-cloud-samples/cloud-config-client/src/main/java/com/bkunzh/springcloud/configdemo/service/GitController.java
@@ -1,6 +1,7 @@
 package com.bkunzh.springcloud.configdemo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author bkunzh
  * @date 2020/10/8
  */
+//@RefreshScope
 @RestController
 public class GitController {
     @Autowired
